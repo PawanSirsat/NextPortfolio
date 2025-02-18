a21323c2270b7996c2020dd849c1cd2ba97c1e0d
PawanSirsat <p1.sirsat1998@gmail.com>
2025-02-18 20:40:32 +0530

    Update TrendingProjects.jsx

--- src/app/(website)/components/TrendingProjects.jsx
+++ src/app/(website)/components/TrendingProjects.jsx
@@ -10,33 +10,28 @@ const TrendingProjects = () => {
   console.log(projects);
 
   return (
-    <>
+    <div className="flex-1 rounded-lg">
       <h2 className="mb-6 text-xl font-bold text-left">Trending Projects</h2>
-      <div className="relative">
-        <div className="flex overflow-x-scroll space-x-6 scrollbar-hide">
-          {isLoading ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-              {Array(4)
-                .fill(0)
-                .map((_, index) => (
-                  <ProjectCardSkeleton key={index} />
-                ))}
-            </div>
-          ) : Array.isArray(projects?.projects) &&
-            projects?.projects.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-              {projects?.projects.map((project, index) => (
-                <ProjectCard key={index} {...project} />
-              ))}
-            </div>
-          ) : (
-            <p className="text-sm text-muted-foreground">
-              No projects yet. Click on add projects.
-            </p>
-          )}
+      {isLoading ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {Array(4)
+            .fill(0)
+            .map((_, index) => (
+              <ProjectCardSkeleton key={index} />
+            ))}
         </div>
-      </div>
-    </>
+      ) : Array.isArray(projects?.projects) && projects?.projects.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          {projects?.projects.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-sm text-muted-foreground">
+          No projects yet. Click on add projects.
+        </p>
+      )}
+    </div>
   );
 };
 
