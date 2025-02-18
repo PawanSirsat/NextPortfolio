dcbb32433ba9160e4d65200da4ff604f4788f923
PawanSirsat <p1.sirsat1998@gmail.com>
2025-02-18 16:04:52 +0530

    Update page.tsx

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,7 +1,5 @@
 import SearchBar from "./components/SearchBar";
 import CategoriesBar from "./components/CategoriesBar";
-import TrendingProjects from "./components/TrendingProjects";
-import LatestArticles from "./components/LatestArticles";
 
 export default function Home() {
   return (
